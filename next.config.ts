012c4f8357a61d6987d3de7c06892be5d1c09cc9
JesusVA760 <tonio4340406@gmail.com>
2025-07-02 13:52:19 -0700

    Change

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@prisma/client"],
+
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // Add Prisma engines to the bundle
-      config.externals.push({
-        "@prisma/client": "@prisma/client",
-      });
-
-      // Copy Prisma engines with proper 'new' constructor
-      const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
-      config.plugins.push(new PrismaPlugin());
+      config.externals.push("@prisma/client");
     }
     return config;
   },
-  // Updated config option for Next.js 15
-  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
