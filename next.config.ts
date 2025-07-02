77bdf401df1456d960422de76edc90ddb44b63c4
JesusVA760 <tonio4340406@gmail.com>
2025-07-02 13:41:55 -0700

    Webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Add Prisma engines to the bundle
+      config.externals.push({
+        "@prisma/client": "@prisma/client",
+      });
+
+      // Copy Prisma engines
+      config.plugins.push(require("@prisma/nextjs-monorepo-workaround-plugin").PrismaPlugin());
+    }
+    return config;
+  },
+  experimental: {
+    // Help Prisma work better in serverless environments
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 };
 
 export default nextConfig;
